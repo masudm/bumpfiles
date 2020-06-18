a95ee9d86ac21401d9c623abd36abf0ce32c5321
Masud Mohammed <0pixelz0@gmail.com>
2020-06-18 11:32:16 +0100

    add help template component

--- bumpfiles/src/components/Bump/Bump.js
+++ bumpfiles/src/components/Bump/Bump.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./Bump.css";
+import Help from "../Help/Help";
 
 const BumpButton = ({ onClick, isListening, time }) => {
 	return (
@@ -13,11 +14,15 @@ const BumpButton = ({ onClick, isListening, time }) => {
 				{/* if the time is more than 0 then it has bumped at least once so we can show info. */}
 				{/* within that check what type of info to display... */}
 				{/* if within an if using ternary expressions */}
-				{time > 0
-					? isListening
-						? "Listening for peers... Bump another device!"
-						: "No peers found. Try again!"
-					: ""}
+				{time > 0 ? (
+					isListening ? (
+						"Listening for peers... Bump another device!"
+					) : (
+						"No peers found. Try again!"
+					)
+				) : (
+					<Help />
+				)}
 			</div>
 		</div>
 	);
---
+++ bumpfiles/src/components/Help/Help.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Help = ({}) => {
+	return <div></div>;
+};
+
+Help.propTypes = {};
+
+export default Help;
