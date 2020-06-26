cc07aacfbffa638512569d33cad0b2540ca0f0ee
Masud Mohammed <0pixelz0@gmail.com>
2020-06-26 10:24:36 +0100

    remove header

--- bumpfiles/src/App.js
+++ bumpfiles/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Transfer } from "./components/Transfer/Transfer";
 
@@ -10,7 +9,6 @@ import "./App.css";
 export default function App() {
 	return (
 		<Router>
-			{/* <Header /> */}
 			<div>
 				{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
