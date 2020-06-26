db74181d3debc1e2b01a96a5e80ba3bfd52e559d
Masud Mohammed <0pixelz0@gmail.com>
2020-06-26 10:31:19 +0100

    change colour to more consistent

--- bumpfiles/src/components/Uploader/Uploader.js
+++ bumpfiles/src/components/Uploader/Uploader.js
@@ -4,21 +4,12 @@ import Files from "react-files";
 import "./Uploader.css";
 
 export default function Uploader({ onChange }) {
-	function onFilesChange(files) {
-		console.log(files);
-	}
-
-	function onFilesError(error, file) {
-		console.log("error code " + error.code + ": " + error.message);
-	}
-
 	return (
 		<div className="files">
 			<Files
 				className="uploader"
 				dropActiveClassName="uploader-drag"
 				onChange={onChange}
-				onError={onFilesError}
 				multiple={false}
 				clickable>
 				Drop your file here or <b>click to browse</b>
